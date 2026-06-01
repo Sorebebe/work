388b7190a5c139ce21e177ddd740ac83f0756545
Tasya Zvereva <tasyazvereva@MacBook-Air-Tasya.local>
2026-06-01 23:46:13 +0800

    Fix: удалены остатки приложения accounts из настроек

--- employment_project/settings.py
+++ employment_project/settings.py
@@ -20,7 +20,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'accounts',
     'vacancies',
     'core',
     'infrastructure',
--- employment_project/urls.py
+++ employment_project/urls.py
@@ -5,5 +5,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('vacancies.urls')),
     path('api/', include('vacancies.api_urls')),
-    path('accounts/', include('accounts.urls')),
 ]
